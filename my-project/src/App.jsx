035187e11f26380ad6cbf5c5bad06b6b6065900e
alone_coder <blackdevil190402@gmail.com>
2025-10-27 22:19:52 +0530

    In-progress

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './Cards/Card.jsx';
+
+function App() {
+
+
+  return (
+    <>
+    <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/Cards/Card.jsx
@@ -0,0 +1,41 @@
+
+
+/*
+{
+    "id": 1,
+    "name": "Cappuccino",
+    "image": "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/cappuccino.jpg",
+    "price": "$5.20",
+    "rating": 4.7,
+    "votes": 65,
+    "popular": true,
+    "available": true
+ */
+
+
+
+import React from "react";
+export default function  Card(props){
+
+    return(
+      <>
+      <div className="card-wrapper">
+          <div className="card-image">
+              <img src = {props.image}  alt="Product not available"/>
+              {props.popular && <h1>popular</h1>}
+
+          </div>
+          <div className="card-content">
+              <h1 className="card-name">{props.name}</h1>
+              <h1>{props.price}</h1>
+              <div className="card-star">
+                  <img src = ""/>
+              </div>
+          </div>
+      </div>
+
+
+      </>
+
+    );
+}
