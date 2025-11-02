795151676d9c5ca782feac5a5a890801f1e488ef
alone_coder <blackdevil190402@gmail.com>
2025-11-02 22:48:31 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-
 import './App.css'
-import Card from './Cards/Card.jsx';
-import Body from  './body/Body.jsx'
+import Card from './Cards/Card.jsx'
+import Body from './body/Body.jsx'
+import useCoffeeData from './hooks/Api.js'
+
 function App() {
 
-    const  data = {
-        "id": 1,
-        "name": "Cappuccino",
-        "image": "./src/assets/bg-cafe.jpg",
-        "price": "$5.20",
-        "rating": 4.7,
-        "votes": 65,
-        "popular": true,
-        "available": false
-    }
+    const [data, loading, error] = useCoffeeData()
 
-  return (
-    <>
+    if (loading) return (
+        <>
+        <h1>Loading .... </h1>
+        </>
+    );
+    if ( error ) return <p>{error}</p>;
 
-        <div className="card">
-            <Card {...data}/>
-        </div>
-    </>
-  )
+
+
+    return (
+        <>
+
+
+        </>
+    );
 }
 
 export default App
