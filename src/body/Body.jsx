0ea0dbeef77fdfe8e1a33c73070af0018ab1fd9b
alone_coder <blackdevil190402@gmail.com>
2025-11-07 13:18:38 +0530

    deployment is pending

--- src/body/Body.jsx
+++ src/body/Body.jsx
@@ -5,13 +5,9 @@ import Display from "../Listing/Display.jsx";
 export default function Body() {
     return (
         <div className={style.wrapper}>
-            <div className={style.topBackImage}>
-                <img src={bgImage} alt="bg-cafe" />
 
-            </div>
             <div className={style.collection}>
                     <Display/>
-
             </div>
 
 
